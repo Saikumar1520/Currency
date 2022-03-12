6490c411389e7c74ad39a3ecda275832f3cd6ed6
sai1512 <sainaidu1512@gmail.com>
2022-03-12 22:20:25 +0530

    Initial commit

---
+++ src/main/java/Currency.java
@@ -0,0 +1,13 @@
+public class Currency {
+    public static int dollar(int rupee){
+        return rupee/78;
+    }
+
+    public static int euro(int rupee){
+        return rupee/86;
+    }
+
+    public static int pound(int rupee){
+        return rupee/102;
+    }
+}
---
+++ src/test/java/CurrencyTest.java
@@ -0,0 +1,27 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CurrencyTest {
+
+    public static Currency Currency;
+    @BeforeClass
+    public static void setup(){
+        Currency = new Currency();
+    }
+    @Test
+    public void testdollar(){
+        assertEquals(2, Currency.dollar(156));
+    }
+
+    @Test
+    public void testeuro(){
+        assertEquals(2, Currency.euro(172));
+    }
+
+    @Test
+    public void testpound(){
+        assertEquals(2, Currency.pound(204));
+    }
+}
